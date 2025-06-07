3f7993040801e5a1e862f99d8b491ea91030683b
Shreya <Shreya@LAPTOP-RGN3DGNM.mshome.net>
2025-06-07 14:26:59 +0530

    commit

---
+++ Automation_FremworkQSP/src/test/java/Eeranna/ForkAndPullRequest.java
@@ -0,0 +1,12 @@
+package Eeranna;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ForkAndPullRequest {
+	@Test
+	public void demo() {
+		Reporter.log("ForkAndPullRequest added",true);
+	}
+
+}
